487c2c6704109a70dd0f1e3fcea65ed16a003417
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2022-11-27 19:19:37 +0400

    fix prod public path

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,4 +1,4 @@
-const {merge} = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -7,17 +7,18 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
-      shared: packageJson.dependencies
-    })
-  ]
-}
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
 
 module.exports = merge(commonConfig, prodConfig);
